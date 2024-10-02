35473bb8c72342e1713bbe010b8d613f971a8fd2
Daniil Pshenik <dpshenik2002@gmail.com>
2024-10-03 00:00:46 +0300

    lol))

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,9 @@
 if ! command -v brew &>/dev/null; then
   echo "Installing Homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo >> "$HOME/.zprofile"
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "$HOME/.zprofile"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 else
   echo "Homebrew already installed."
 fi
