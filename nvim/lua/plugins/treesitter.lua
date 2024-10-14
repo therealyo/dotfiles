47378f93acb228d71422e9855d3a8f2575581f34
Daniil Pshenik <therealyo@MBP-Daniil.lan>
2024-10-14 22:28:24 +0300

    add treesitter context and zen mode

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,36 +1,38 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	main = "nvim-treesitter.configs", -- Sets main module to use for opts
-	-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-	opts = {
-		ensure_installed = {
-			"bash",
-			"c",
-			"diff",
-			"html",
-			"lua",
-			"luadoc",
-			"markdown",
-			"markdown_inline",
-			"query",
-			"vim",
-			"vimdoc",
-			"elixir",
-			"eex",
-			"erlang",
-			"ruby",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		main = "nvim-treesitter.configs", -- Sets main module to use for opts
+		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+		opts = {
+			ensure_installed = {
+				"bash",
+				"c",
+				"diff",
+				"html",
+				"lua",
+				"luadoc",
+				"markdown",
+				"markdown_inline",
+				"query",
+				"vim",
+				"vimdoc",
+				"elixir",
+				"eex",
+				"erlang",
+				"ruby",
+			},
+			-- Autoinstall languages that are not installed
+			auto_install = true,
+			highlight = {
+				enable = true,
+				-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+				--  If you are experiencing weird indenting issues, add the language to
+				--  the list of additional_vim_regex_highlighting and disabled languages for indent.
+				additional_vim_regex_highlighting = { "ruby" },
+			},
+			indent = { enable = true, disable = { "ruby" } },
 		},
-		-- Autoinstall languages that are not installed
-		auto_install = true,
-		highlight = {
-			enable = true,
-			-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-			--  If you are experiencing weird indenting issues, add the language to
-			--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-			additional_vim_regex_highlighting = { "ruby" },
-		},
-		indent = { enable = true, disable = { "ruby" } },
 	},
 	{
 		"RRethy/nvim-treesitter-endwise",
@@ -60,5 +62,31 @@ return {
 				},
 			},
 		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		opts = function()
+			local tsc = require("treesitter-context")
+
+			local function toggle_treesitter_context()
+				if tsc.is_enabled() then
+					tsc.disable()
+					print("Treesitter Context disabled")
+				else
+					tsc.enable()
+					print("Treesitter Context enabled")
+				end
+			end
+
+			-- Map the keybinding to toggle Treesitter context
+			vim.api.nvim_set_keymap(
+				"n",
+				"<leader>ut",
+				"<cmd>lua toggle_treesitter_context()<CR>",
+				{ noremap = true, silent = true, desc = "Toggle Treesitter Context" }
+			)
+
+			return { mode = "cursor", max_lines = 3 }
+		end,
 	},
 }
