4b212e65049c12728caa0d8de22abf4c17ea7674
therealyo <dpshenik2002@gmail.com>
2024-10-24 21:36:22 +0300

    some more ui changes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -42,7 +42,7 @@ config.font = wezterm.font_with_fallback({
 	"monospace",
 })
 
-config.line_height = 1.3
+config.line_height = 1.1
 config.cell_width = 1.0
 config.font_size = 12
 config.harfbuzz_features = { "calt=1", "clig=1", "liga=1" } -- Enable font ligatures
