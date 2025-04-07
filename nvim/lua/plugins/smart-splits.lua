711446b8f9494d87a969c208d3243ce8bd2f8d7a
Daniil Pshenik <dpshenik2002@gmail.com>
2025-04-07 14:02:39 +0300

    fix splits

--- nvim/lua/plugins/smart-splits.lua
+++ nvim/lua/plugins/smart-splits.lua
@@ -25,10 +25,10 @@ return {
 		vim.keymap.set("n", "<C-A-k>", require("smart-splits").resize_up)
 		vim.keymap.set("n", "<C-A-l>", require("smart-splits").resize_right)
 		-- moving between splits
-		vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
-		vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
-		vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
-		vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
+		vim.keymap.set("n", "<C-w-h>", require("smart-splits").move_cursor_left)
+		vim.keymap.set("n", "<C-w-j>", require("smart-splits").move_cursor_down)
+		vim.keymap.set("n", "<C-w-k>", require("smart-splits").move_cursor_up)
+		vim.keymap.set("n", "<C-w-l>", require("smart-splits").move_cursor_right)
 		vim.keymap.set("n", "<C-\\>", require("smart-splits").move_cursor_previous)
 		-- swapping buffers between windows
 		vim.keymap.set("n", "<leader><leader>h", require("smart-splits").swap_buf_left)
